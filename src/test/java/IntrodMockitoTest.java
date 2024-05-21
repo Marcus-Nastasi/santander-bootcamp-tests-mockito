fbcd39f0b4de44eadd46f190413dd85d8b5bf746
rolemberg <vinnie.nsts@gmail.com>
2024-05-21 10:46:05 -0300

    testing first addSomethin with mockito;

--- src/test/java/IntrodMockitoTest.java
+++ src/test/java/IntrodMockitoTest.java
@@ -1,8 +1,24 @@
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.List;
 
 @ExtendWith(MockitoExtension.class)
 public class IntrodMockitoTest {
 
+    @Mock
+    private List<String> letters;
 
+    @Test
+    void addSomething() {
+        Mockito.when(letters.getFirst()).thenReturn("B");
+        assertEquals("B", letters.getFirst());
+    }
 }
+
+
